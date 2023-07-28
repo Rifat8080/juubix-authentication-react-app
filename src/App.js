b15b4f0f092322255355b314d90f78a7bc32025e
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-07-29 00:01:02 +0600

    Updated the app js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Registration from './Registration';
+import Login from './Login';
 
 function App() {
   return (
     <main className="App">
-      <Registration />
+      <Login />
     </main>
   );
 }
